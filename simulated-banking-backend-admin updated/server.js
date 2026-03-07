55e6209214d5ea380aba604e8f4c5dbbcba49ba1
alisonbartey <alisonbartey@gmail.com>
2026-03-06 16:11:15 -0800

    Update server.js

--- simulated-banking-backend-admin updated/server.js
+++ simulated-banking-backend-admin updated/server.js
@@ -38,7 +38,7 @@ app.use(helmet({
 
 // ✅ CORS
 const allowedOrigins = [
-  'https://wells-fargo-online-banking.vercel.app',
+  'https://well-fargo-online-banking.vercel.app',
   'http://localhost:3000',
   'http://127.0.0.1:5500',
   'null'
